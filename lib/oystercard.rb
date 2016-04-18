0c7cf52eb94db4c3e13efce1cd9fa83312c3c16e
gerauf <fo4786@gmail.com>
2016-04-18 17:12:03 +0100

    max balance added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,17 +1,22 @@
 class Oystercard
   attr_reader :balance
 
+  DEFAULT_STARTING_BALANCE = 0
+  MAX_BALANCE = 90
+
   def initialize(starting_balance = DEFAULT_STARTING_BALANCE)
     @balance = starting_balance
   end
 
-  def add(cash)
+  def top_up(cash)
+    fail "Feck off" if exceeds_max_balance?(cash)
     @balance += cash
   end
 
 
   private
-
-  DEFAULT_STARTING_BALANCE = 0
+  def exceeds_max_balance?(cash)
+    (balance + cash) > MAX_BALANCE
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,24 +1,29 @@
 require 'oystercard'
 
 describe Oystercard do
-  let (:empty_oystercard) { Oystercard.new }
-  let (:oystercard) { Oystercard.new(10) }
+  let (:oystercard) { Oystercard.new }
+  let (:oystercard_with10) { Oystercard.new(10) }
 
   describe '#balance' do
     it 'new oystercards have a default 0 balance' do
-      expect(empty_oystercard.balance).to eq 0
+      expect(oystercard.balance).to eq 0
     end
 
     it 'new oystercards can be created with a starting balance' do
-      expect(oystercard.balance).to eq 10
+      expect(oystercard_with10.balance).to eq 10
     end
   end
 
-  describe '#add' do
-    it 'money added should show up on balance' do
-      empty_oystercard.add(10)
-      expect(empty_oystercard.balance).to eq 10
+  describe '#top_up' do
+    it 'add to balance on card' do
+      expect{ oystercard.top_up 1 }.to change{ oystercard.balance }.by 1
     end
+    it 'raises error if max balance exceeded' do
+      max_balance = Oystercard::MAX_BALANCE
+      oystercard.top_up max_balance
+      expect{oystercard.top_up 1}.to raise_error "Feck off"
+    end
+
   end
 
 end
