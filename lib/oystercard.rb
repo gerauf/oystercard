a57f6229cd0ce802fec1147c831498b9b4ee2fca
Mara Wanot <m.d.wanot@hotmail.com>
2016-04-19 11:24:44 +0100

    add touch in, touch out and in journey methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,6 +5,19 @@ class Oystercard
 
 	def initialize
 		@balance = 0
+		@in_journey = false
+	end
+
+	def in_journey?
+		in_journey
+	end
+
+	def touch_in
+		@in_journey = true
+	end
+
+	def touch_out
+		@in_journey = false
 	end
 
 	def top_up(amount)
@@ -17,9 +30,9 @@ class Oystercard
   end
 
   private
+  attr_reader :in_journey
   def exceeded_max?(amount)
     (balance + amount) > MAX_BALANCE
   end
-
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,30 +4,48 @@ describe Oystercard do
 
 	let(:oyster) { Oystercard.new }
 
-	it { is_expected.to respond_to(:deduct).with(1).argument }
-
 	describe "#initalize" do
-		it "has a default balance of £0" do
+		it "default balance is £0" do
 			expect(oyster.balance).to eq 0
 	  end
 	end
 
 	describe "#top_up" do
-		it "allows putting money on oystercard" do
+		it "adds money to oystercard" do
 			expect { oyster.top_up 1 }.to change{ oyster.balance }.by 1
 			# expect(Oystercard.new.top_up(3)).to eq 3
 		end
-		it "can only top up to a maximum of £90" do
+		it "top up maximum of £90" do
 			oyster.top_up(90)
-			expect{ oyster.top_up(6) }.to raise_error "Can't top up over £90"
+			expect { oyster.top_up(6) }.to raise_error "Can't top up over £90"
 		end
 	end
 
-	describe '#deduct' do
-		it 'reduces balance by value of fare' do
+	describe "#deduct" do
+		it "reduces balance by value of fare" do
 			expect { oyster.deduct 1 }.to change{ oyster.balance }.by -1
 			expect { oyster.deduct 2 }.to change{ oyster.balance }.by -2
 		end
 	end
 
+	describe "#in_journey?" do
+		it "new oystercard not in journey" do
+			expect(oyster).not_to be_in_journey
+		end
+		
+		context "#touch_in" do
+			before {oyster.touch_in}
+
+			it "in journey" do
+				expect(oyster).to be_in_journey
+			end
+
+			context "#touch_out" do
+				before {oyster.touch_out}
+				it "not in journey" do
+					expect(oyster).not_to be_in_journey
+				end
+			end
+		end
+	end
 end
